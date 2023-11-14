20ac5f85f1362abb014589b7ebf33179a79fbdd0
Josh Kersker <jfk17a@acu.edu>
2023-11-14 11:58:49 -0600

    Remove currently unused code, will add back later.

--- DamageCalc.py
+++ DamageCalc.py
@@ -3,20 +3,6 @@ rapierDmg = (2,6)
 
 def main():
     print(DmgAvr(rapierDmg))
-    # print(CrtDmgAvr(rapierDmg))
-    # print(ResDmgAvr(rapierDmg))
-    
-def CrtDmgAvr(wepDmg):
-    count, size = wepDmg
-    count += count
-    dmgChance = []
-    for rolls in range(1,count+1):
-        for options in range(1,size+1):
-            # print("Die:", rolls)
-            # print("Roll:",options)
-            dmgChance.append(options)
-    avg = sum(dmgChance)/len(dmgChance)
-    return avg
 
 def DmgAvr(wepDmg):
     dmgchance = []
@@ -30,17 +16,6 @@ def DmgAvr(wepDmg):
             dieCount.append(dmgchance)
         dieOp = dieOptions(dieCount)
         return dieCount
-
-def ResDmgAvr(wepDmg):
-    count, size = wepDmg
-    dmgChance = []
-    for rolls in range(1,count+1):
-        dmgChance.append(dieRollOp(size))
-    dmgLst = []
-    
-    # avg = sum(dmgChance)/len(dmgChance)
-    # avgRes = avg/2
-    return dmgLst
 
 def dieOptions(dieArray,):
     pass
