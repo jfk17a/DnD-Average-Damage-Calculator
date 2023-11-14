d3cfa377add263d1f35de9e19beb1de0a4302f4c
Josh Kersker <jfk17a@acu.edu>
2023-11-14 14:28:48 -0600

    Add a text interface for basic use cases.

--- DamageCalc.py
+++ DamageCalc.py
@@ -1,8 +1,22 @@
-
-rapierDmg = (3,6)
-
 def main():
-    print(DmgCrt(rapierDmg))
+    dmg = int(input("Please provide an acceptable size for the damage die: "))
+    size = int(input("Please provide how many dice will be rolled: "))
+    wepDmgStart = (size,dmg)
+    option = ""
+    passed = False
+    while option.upper() != "C" and option.upper() != "A" and option.upper() != "R":
+        if passed == False:
+            option = input("What would you like to do with this damage roll? \nResisted Damage (R), Critical Damage (C), or Average Damage(A)?")
+            passed = True
+        else:
+            option = input("That was an invalid option, please select an appropriate option. \nResisted Damage (R), Critical Damage (C), or Average Damage(A)?")
+    match option.upper():
+        case "A":
+            print(DmgAvr(wepDmgStart))
+        case "C":
+            print(DmgCrt(wepDmgStart))
+        case "R":
+            print(DmgRes(wepDmgStart))
 
 def DmgAvr(wepDmg):
     return ((wepDmg[1]+1)/2)*wepDmg[0]
