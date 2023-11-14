a0b1fc417894d3a5067dc87102668081f93231ed
Josh Kersker <jfk17a@acu.edu>
2023-11-14 14:01:26 -0600

    Refactored code to use math formulas rather than deriving the calculation manually.

--- DamageCalc.py
+++ DamageCalc.py
@@ -1,30 +1,17 @@
 
-rapierDmg = (2,6)
+rapierDmg = (3,6)
 
 def main():
-    print(DmgAvr(rapierDmg))
+    print(DmgCrt(rapierDmg))
 
 def DmgAvr(wepDmg):
-    dmgchance = []
-    dieCount = []
-    for i in range(0,wepDmg[1]):
-            dmgchance.append(i+1)
-    if wepDmg[0] == 1:
-        return sum(dmgchance)/len(dmgchance)
-    else:
-        for i in range(0,wepDmg[0]):
-            dieCount.append(dmgchance)
-        dieOp = dieOptions(dieCount)
-        return dieCount
+    return ((wepDmg[1]+1)/2)*wepDmg[0]
 
-def dieOptions(dieArray,):
-    pass
+def DmgRes(wepDmg):
+    return DmgAvr(wepDmg)/2
 
-def dieRollOp(size):
-    lst = []
-    for options in range(1,size+1):
-        lst.append(options)
-    return lst
+def DmgCrt(wepDmg):
+    return DmgAvr(wepDmg)*2
 
 if __name__ == "__main__":
     main()
